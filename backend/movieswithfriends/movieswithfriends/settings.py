a62a32e1523c4dcd0bbe0db8a03099f3f3517c8c
xenine <ivanmyazin99@gmail.com>
2023-03-14 05:22:22 +0300

    added hosts

--- backend/movieswithfriends/movieswithfriends/settings.py
+++ backend/movieswithfriends/movieswithfriends/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.filmfriends.online', '.localhost', '127.0.0.1']
 
 
 # Application definition
